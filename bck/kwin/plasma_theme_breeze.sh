881d1c87846b1d407e64d8e139df23456f0d627b
a <a@a>
2024-11-23 22:11:32 +0800

    kwinbackup

--- bck/kwin/plasma_theme_breeze.sh
+++ bck/kwin/plasma_theme_breeze.sh
@@ -6,6 +6,7 @@
 #这里不用绝对路径,kwrite不会接收
 meta=~/.local/share/plasma/desktoptheme/default/metadata.desktop 
 
+mkdir -p ~/.local/share/plasma/desktoptheme/
 #修改breeze中的contrast
 if [ -f $meta ];then
     echo exist
@@ -15,11 +16,11 @@ fi
 
 
 #以下基本上是一个类同air的白色主题,基本上加强饱和度,都会使得颜色变亮
-kwriteconfig5 --file $meta --group ContrastEffect --key enabled true
+kwriteconfig6 --file $meta --group ContrastEffect --key enabled true
 #对比度,越低越亮
-kwriteconfig5 --file $meta --group ContrastEffect --key contrast 1.0
-kwriteconfig5 --file $meta --group ContrastEffect --key intensity 1.2
+kwriteconfig6 --file $meta --group ContrastEffect --key contrast 1.0
+kwriteconfig6 --file $meta --group ContrastEffect --key intensity 1.2
 #2.0是个差不多的数值,饱和度更高就会出现明显色块,颜色过度不再平滑
-kwriteconfig5 --file $meta --group ContrastEffect --key saturation 2.0
+kwriteconfig6 --file $meta --group ContrastEffect --key saturation 2.0
 
 
