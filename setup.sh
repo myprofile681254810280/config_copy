6880c33f832392889fd747a4526e45e2bab18df7
a <a@a>
2024-11-23 20:41:33 +0800

    plasma

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #ROOT="$(dirname "$(realpath "$0")")"
 #echo "$ROOT"
+
 cp kwinrulesrc ~/.config/
+
 rm ~/.config/mpv -r
 cp mpv -r ~/.config
+
+#plasma 相关的一组，似乎不完全是关于plasmashell的，但是应该都有用
+cp plasma-org.kde.plasma.desktop-appletsrc ~/.config/
+cp plasmanotifyrc ~/.config/
+cp plasmaparc ~/.config/
+cp plasmarc ~/.config/
+cp plasmashellrc ~/.config/
