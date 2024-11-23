16a0bc93f60b846c7730e7ac5532e90034750553
a <a@a>
2024-11-23 20:45:49 +0800

    kwinrc

--- setup.sh
+++ setup.sh
@@ -12,3 +12,6 @@ cp plasmanotifyrc ~/.config/
 cp plasmaparc ~/.config/
 cp plasmarc ~/.config/
 cp plasmashellrc ~/.config/
+
+#kwinrc有另外一个对选项注释更清晰，用configwriter写的备份。   但是configwriter还有接口升级之类的问题，反而直接保存的这份更方便，  不过没有注释还是很大的缺点。可以考虑把configwriter的那份留下作为注释，在需要的时候看
+cp kwinrc  ~/.config/
