d527753766bd6817f857198aec7c6795d22e1dc1
a <a@a>
2024-11-24 13:46:15 +0800

    bck

---
+++ bck/vol_dec.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pamixer -u
+pamixer -d 1
+
+#有可能卡屏幕
+#bash ~/app/kwin/volnoti-kwin/show-vol.sh
+
+#mpc volume -1
+
---
+++ bck/vol_inc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pamixer -u
+pamixer -i 1
+#有可能卡屏幕
+#bash ~/app/kwin/volnoti-kwin/show-vol.sh
+
+#mpc volume +1
